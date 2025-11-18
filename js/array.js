4f1d0aa36f68c4b409283a77b0621dbb0c60e71b
정그린 <th2gr22n@gmail.com>
2025-11-18 23:00:22 +0900

    배열 - filter, map 학습

--- js/array.js
+++ js/array.js
@@ -20,8 +20,87 @@ const findIdx = pid => () =>
 // const findId = pid => user => user.id === pid;
 const findId =
   pid =>
-  ({ id }) => id === pid;
+  ({ id }) =>
+    id === pid;
 const idxId11 = users.findLastIndex(findId(1));
 // const idxId11 = users.findLastIndex(a => a.id === 1);
 // console.log(users);
 console.log('🚀  idxId11:', idxId11);
+
+//              0  1  2  3  4
+// const arr = [1, 2, 3, 4, 5];
+const arr = Array.from({length: 5}, (_, i) => i + 1);
+
+const isEven = n => n % 2 === 0;
+const ev1 = arr.map((_, i) => isEven(i));
+console.log("🚀 ~ ev1:", ev1);
+const ev2 = arr.map(isEven);
+console.log("🚀 ~ ev2:", ev2);
+// const onlyEvens = arr.filter(a => a % 2 === 0);
+const onlyEvens = arr.filter(isEven);
+console.log("🚀 ~ onlyEvens:", onlyEvens);
+arr.forEach(a => console.log(a, isEven(a)));
+for(let i = 0; i < arr.length; i++) console.log(arr[i], isEven(arr[i])); // for보다 forEach를 더 많이 씀
+for(const a of arr) {
+  console.log(a, isEven(a));
+  if (a === 3) break;
+}
+const arr2 = [...arr];
+console.log("🚀 ~ arr2:", arr2 === arr);
+const arr3 = arr2.concat(arr);
+console.log("🚀 ~ arr3:", arr3);
+const arr4 = [...arr2, ...arr];
+console.log("🚀 ~ arr4:", arr4);
+
+const a3 = arr.find(a => a === 3);
+console.log("🚀 ~ a3:", a3)
+const evenOdds = Object.groupBy(arr, (a) => isEven(a) ? 'even' : 'odd');
+console.log("🚀 ~ evenOdds:", evenOdds);
+const jarr = arr.join('');
+console.log("🚀 ~ jarr:", jarr);
+
+//         0  1  2  3  4  5  6
+const a = [1, 2, 3, 4, 5, 6, 7];
+//               3, 4, 5, 6, 7
+
+//               3, 4,
+//        [1, 2, 3, 4, 3, 4, 7];
+a.copyWithin(4, 2, 4);
+console.log("🚀 ~ a:", a);
+a.push('02', '01', '03', 'a', 'c', 'b', 'ba', '다');
+let b = a;
+const s1 = b.sort();
+console.log("🚀 ~ s1:", s1);
+b = a;
+const s2 = b.sort((a, b) => b - a);
+console.log("🚀 ~ s2:", s2);
+b = a;
+const s3 = b.sort((a, b) => (a < b ? -1 : 1));
+console.log("🚀 ~ s3:", s3);
+b = a;
+const s4 = b.sort((a, b) => (a > b ? 1 : -1));
+console.log("🚀 ~ s4:", s4);
+
+[users[1], users[2]] = [users[2], users[1]];
+console.log(users);
+// const us1 = users.sort((a, b) => a.id - b.id);
+const us1 = users.sort(({ id }, { id: id2 }) => id - id2);
+console.log("🚀 ~ us1:", us1);
+console.log('------------', arr2);
+const shallow = arr2.slice();
+console.log("🚀 ~ shallow:", shallow);
+const shallow2 = [...arr2];
+console.log("🚀 ~ shallow2:", shallow2);
+console.log('------------', arr2);
+//   0, 1, 2, 3, 4
+// [ 1, 2, 3, 4, 5]
+const sp1 = arr2.splice(1, 3);
+console.log("🚀 ~ sp1:", sp1, arr2);
+// [1, 5]
+// arr2.splice(1, 0, 2, 3, 4);
+arr2.splice(1, 0, ...sp1);
+console.log("🚀 ~ arr2:", arr2);
+const sp2 = arr.splice(2);
+console.log("🚀 ~ arr2:", arr2);
+arr2.splice(2, 0, ...sp2);
+console.log("🚀 ~ arr2:", arr2);
