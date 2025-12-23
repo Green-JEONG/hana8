1fc818710f5b8bffe489110a23a79d375f6f7036
정그린 <th2gr22n@gmail.com>
2025-12-23 17:21:33 +0900

    Next - 환경설정, Routes 14

---
+++ next/my-app/app/parallel/layout.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  login: ReactNode;
+  profile: ReactNode;
+};
+export default function ParallelLayout({ children, login, profile }: Props) {
+  return (
+    <div>
+      <h1 className="text-center">Parallel Layout</h1>
+      <div className="flex gap-3">
+        <Link href="/parallel/aaa">AAA</Link>
+        <Link href="/parallel/bbb">BBB</Link>
+      </div>
+
+      <div>{children}</div>
+
+      {/* .grid */}
+      <div className="grid grid-cols-2 gap-3">
+        <div className="border">{profile}</div>
+        <div className="border">{login}</div>
+      </div>
+    </div>
+  );
+}
